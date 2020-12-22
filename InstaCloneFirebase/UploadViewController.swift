ba83e2dc0372e90108ccd9a36e5d663421d144d4
the-hyperborean <jtb@abaris.com.au>
2020-12-23 10:43:20 +1100

    Finished Section

--- InstaCloneFirebase/UploadViewController.swift
+++ InstaCloneFirebase/UploadViewController.swift
@@ -71,12 +71,18 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
                             
                             var firestoreReference : DocumentReference? = nil
                             
-                            let firestorePost = ["imageURL" : imageUrl!, "postedBy" : Auth.auth().currentUser!.email!, "postComment" : self.commentText.text!,"date" : "date", "likes" : 0 ] as [String : Any]
+                            let firestorePost = ["imageURL" : imageUrl!, "postedBy" : Auth.auth().currentUser!.email!, "postComment" : self.commentText.text!,"date" : FieldValue.serverTimestamp(), "likes" : 0 ] as [String : Any]
                             
                             firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { (error) in
                                 if error != nil {
                                     
                                     self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                                    
+                                } else {
+                                    
+                                    self.imageView.image = UIImage(named: "select.png")
+                                    self.commentText.text = ""
+                                    self.tabBarController?.selectedIndex = 0 // Feed
                                     
                                 }
                             })
